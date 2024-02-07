48ab6b52792c187665b1e35805073ea1f62e4a0b
Rahul6819 <156073410+Rahul6819@users.noreply.github.com>
2024-02-07 23:54:12 +0530

    Add files via upload

---
+++ 10/codes/graph5.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Read data from file
+with open('data5.txt', 'r') as file:
+    data = [int(line.strip()) for line in file]
+
+# Plot the data
+plt.plot(range(len(data)), data, marker='o', linestyle='-')
+plt.title('Arithmetic Progression')
+plt.xlabel('n')
+plt.ylabel('x(n)')
+plt.grid(True)
+plt.show()
